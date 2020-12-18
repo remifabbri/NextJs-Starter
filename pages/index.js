cca352b6779ee5016e77bc43de83372da5c47f7a
krash <krash@MacBook-Pro-de-remi.local>
2020-12-18 17:21:25 +0100

    debug after clean

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,16 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
-import Date from '../components/date'
-import { getSortedPostsData } from '../lib/posts'
+// import { getSortedPostsData } from '../lib/posts'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData()
+//   return {
+//     props: {
+//       allPostsData
+//     }
+//   }
+// }
 
 export default function Home({allPostsData}) {
 
